c5e3ee12ba7986dab62eee6c9e343c438093336d
Caesar LaVey <dan@tangible.store>
2023-11-16 16:25:52 -0700

    chore: added missing function in IUSTB interface

--- src/interfaces/IUSTB.sol
+++ src/interfaces/IUSTB.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+interface IUSTB {
+    function UNDERLYING() external view returns (address);
 
-interface IUSTB is IERC20 {
-    function mint(address to, uint256 amount) external;
     function burn(address from, uint256 amount) external;
+    function mint(address to, uint256 amount) external;
 
-    function disableRebase(address account, bool disable) external;
+    function rebaseIndexManager() external view returns (address _rebaseIndexManager);
 
-    function setRebaseIndex(uint256 index, uint256 nonce) external;
+    function refreshRebaseIndex() external;
+
     function setRebaseIndexManager(address manager) external;
-
-    function rebaseIndexManager() external returns (address manager);
 }
