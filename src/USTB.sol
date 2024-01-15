debc58577bfac0eb3f9c22ab2aab3db88182cdb8
c-note <dongustavo20@gmail.com>
2024-01-15 08:44:43 +0000

    Remove errors, events, struct, comments on public function to `IUSTB`.

--- src/USTB.sol
+++ src/USTB.sol
@@ -28,11 +28,6 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
 
     address public immutable UNDERLYING;
 
-    /// @custom:storage-location erc7201:tangible.storage.USTB
-    struct USTBStorage {
-        address rebaseIndexManager;
-    }
-
     // keccak256(abi.encode(uint256(keccak256("tangible.storage.USTB")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 private constant USTBStorageLocation = 0x56cb630b12f1f031f72de1d734e98085323517cc6515c1c85452dc02f218dd00;
 
@@ -42,13 +37,6 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
             $.slot := USTBStorageLocation
         }
     }
-
-    event RebaseIndexManagerUpdated(address manager);
-
-    error InvalidZeroAddress();
-    error NotAuthorized(address caller);
-    error UnsupportedChain(uint256 chainId);
-    error ValueUnchanged();
 
     modifier onlyIndexManager() {
         USTBStorage storage $ = _getUSTBStorage();
@@ -80,13 +68,7 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    /**
-     * @notice Initializes the USTB contract with essential parameters.
-     * @dev This function sets the initial LayerZero endpoint and the rebase index manager. It also calls
-     * `__LayerZeroRebaseToken_init` for further initialization.
-     *
-     * @param indexManager The address that will manage the rebase index.
-     */
+    /// @inheritdoc IUSTB
     function initialize(address indexManager) external reinitializer(2) {
         __LayerZeroRebaseToken_init(msg.sender, "US T-Bill", "USTB");
         if (isMainChain) {
@@ -97,29 +79,13 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
         setRebaseIndexManager(indexManager);
     }
 
-    /**
-     * @notice Mints a specified amount of USTB tokens to a given address.
-     * @dev This function first transfers the underlying asset from the caller to the contract. Then, it mints the
-     * equivalent amount of USTB tokens to the target address. The function can only be called if the contract is on the
-     * main chain. It also updates the rebase index before minting.
-     *
-     * @param to The address to which the tokens will be minted.
-     * @param amount The amount of tokens to mint.
-     */
+    /// @inheritdoc IUSTB
     function mint(address to, uint256 amount) external mainChain(true) {
         _mint(to, amount);
         IERC20(UNDERLYING).safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    /**
-     * @notice Burns a specified amount of USTB tokens from a given address.
-     * @dev This function first burns the specified amount of USTB tokens from the target address. Then, it transfers
-     * the equivalent amount of the underlying asset back to the caller. The function can only be called if the contract
-     * is on the main chain. It also updates the rebase index before burning.
-     *
-     * @param from The address from which the tokens will be burned.
-     * @param amount The amount of tokens to burn.
-     */
+    /// @inheritdoc IUSTB
     function burn(address from, uint256 amount) external mainChain(true) {
         if (from != msg.sender) {
             _spendAllowance(from, msg.sender, amount);
@@ -128,13 +94,7 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
         IERC20(UNDERLYING).safeTransfer(msg.sender, amount);
     }
 
-    /**
-     * @notice Enables or disables rebasing for a specific account.
-     * @dev This function can be called by either the account itself or the rebase index manager.
-     *
-     * @param account The address of the account for which rebasing is to be enabled or disabled.
-     * @param disable A boolean flag indicating whether to disable (true) or enable (false) rebasing for the account.
-     */
+    /// @inheritdoc IUSTB
     function disableRebase(address account, bool disable) external {
         USTBStorage storage $ = _getUSTBStorage();
         if (msg.sender != account && msg.sender != $.rebaseIndexManager) {
@@ -146,35 +106,18 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
         _disableRebase(account, disable);
     }
 
+    /// @inheritdoc IUSTB
     function rebaseIndexManager() external view override returns (address _rebaseIndexManager) {
         USTBStorage storage $ = _getUSTBStorage();
         _rebaseIndexManager = $.rebaseIndexManager;
     }
 
-    /**
-     * @notice Sets the rebase index and its corresponding nonce on non-main chains.
-     * @dev This function allows the rebase index manager to manually update the rebase index and nonce when not on the
-     * main chain. The main chain manages the rebase index automatically within `refreshRebaseIndex`. It should only be
-     * used on non-main chains to align them with the main chain's state.
-     *
-     * Reverts if called on the main chain due to the `mainChain(false)` modifier.
-     *
-     * @param index The new rebase index to set.
-     * @param nonce The new nonce corresponding to the rebase index.
-     */
+    /// @inheritdoc IUSTB
     function setRebaseIndex(uint256 index, uint256 nonce) public onlyIndexManager mainChain(false) {
         _setRebaseIndex(index, nonce);
     }
 
-    /**
-     * @notice Updates the rebase index to the current index from the underlying asset on the main chain.
-     * @dev Automatically refreshes the rebase index by querying the current reward multiplier from the underlying asset
-     * contract. This can only affect the rebase index on the main chain. If the current index from the underlying
-     * differs from the stored rebase index, it updates the rebase index and sets the current block number as the nonce.
-     *
-     * This function does not have effect on non-main chains as their rebase index and nonce are managed through
-     * `setRebaseIndex`.
-     */
+    /// @inheritdoc IUSTB
     function refreshRebaseIndex() public {
         if (isMainChain) {
             uint256 currentIndex = IUSDM(UNDERLYING).rewardMultiplier();
@@ -184,13 +127,7 @@ contract USTB is IUSTB, LayerZeroRebaseTokenUpgradeable, UUPSUpgradeable {
         }
     }
 
-    /**
-     * @notice Sets the address of the rebase index manager.
-     * @dev This function allows the contract owner to change the rebase index manager, who has the permission to update
-     * the rebase index.
-     *
-     * @param manager The new rebase index manager address.
-     */
+    /// @inheritdoc IUSTB
     function setRebaseIndexManager(address manager) public onlyOwner {
         if (manager == address(0)) {
             revert InvalidZeroAddress();
